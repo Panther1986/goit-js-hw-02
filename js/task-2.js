d6b76ab177dfa035f23edf5b6e3c6f1d7bd46460
Panther1986 <polzovatel77@gmail.com>
2023-11-18 22:32:58 +0200

    update

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+function calculateTotal(number) {
+   let count = number + number;
+    while (count) {
+        count + number;
+        console.log(count);
+    }
+    
+}
+calculateTotal(1);
+calculateTotal(3);
